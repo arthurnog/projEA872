234c328526bbae95e21696d1346f741916191270
arthur.nogueira <arthur.nogueira@kaffa.com.br>
2021-09-22 08:22:38 -0300

    implementing controller, errors fixed, problems rendering spites

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <memory>
 
-#include "func.h"
+#include "../include/func.h"
 //#include "View.h"
 //#include "Model.h"
 //#include "Controller.h"
@@ -13,25 +13,29 @@ class Model {
     float x_current;
     float y_current;
   public:
-    Model()
-    float get_x();
+    Model();
+    ~Model();
+    int get_x();
     void set_x(float new_x);
-    float get_y();
+    int get_y();
     void set_y(float new_y);
-}
+};
 
 Model::Model() {
-  this->x_current = 100;
-  this->y_current = 100;
+  this->x_current = 100.0;
+  this->y_current = 100.0;
 }
-float Model::get_x(){return this->x_current;}
-void Model::set_x(float nex_x){this->x_current=new_x;}
-float Model::get_y(){return this->y_current;}
-void Model::set_y(float nex_y){this->y_current=new_y;}
+
+Model::~Model(){}
+
+int Model::get_x(){return this->x_current;}
+void Model::set_x(float new_x){this->x_current=new_x;}
+int Model::get_y(){return this->y_current;}
+void Model::set_y(float new_y){this->y_current=new_y;}
 
 class View{
   private:
-    Model &mdl;
+    Model &model;
     SDL_Renderer* renderer;
     SDL_Window* window;
     SDL_Rect target;
@@ -41,7 +45,7 @@ class View{
     View(Model &model);
     ~View();
     void render();
-}
+};
 //view
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -50,7 +54,6 @@ View::View(Model &model) : model(model){
   // Inicializando o subsistema de video do SDL
   if ( SDL_Init (SDL_INIT_VIDEO) < 0 ) {
     std::cout << SDL_GetError();
-    return 1;
   }
 
   // Criando uma janela
@@ -83,16 +86,17 @@ View::View(Model &model) : model(model){
   this->background = IMG_LoadTexture(renderer, "../assets/park.jpeg");
 
   // Quadrado onde a textura sera desenhada
-  this->target.x = model.get_x;
-  this->target.y = model.get_y;
-  SDL_QueryTexture(this->texture, nullptr, nullptr, &(this->target.w), &(this->target.h));
+  this->target.x = model.get_x();
+  this->target.y = model.get_y();
+  SDL_QueryTexture(this->sprite_0, nullptr, nullptr, &(this->target.w), &(this->target.h));
 
 }
 
 View::~View(){
-  SDL_DestroyTexture(texture);
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
+  SDL_DestroyTexture(this->sprite_0);
+  SDL_DestroyTexture(this->background);
+  SDL_DestroyRenderer(this->renderer);
+  SDL_DestroyWindow(this->window);
   SDL_Quit();
 }
 
@@ -108,34 +112,65 @@ void View::render(){
   SDL_Delay(10);
 
 }
-//=============>
-int main() {
-  estou_aqui();
-  //======================>
 
-  // Controlador:
-  bool rodando = true;
+class Controller{
+  private:
+    bool on;
+    SDL_Event event;
+    Model &model;
+    const Uint8* state;
+  public:
+    Controller(Model &model);
+    ~Controller();
+    void polling();
+    bool get_on();
+};
 
-  // Variaveis para verificar eventos
-  SDL_Event evento; // eventos discretos
-  const Uint8* state = SDL_GetKeyboardState(nullptr); // estado do teclado
+Controller::Controller(Model &model) : model(model){
+  this->on = true;
+  this->state = SDL_GetKeyboardState(nullptr);
+}
 
-  // Laco principal do jogo
-  while(rodando) {
-    // Polling de eventos
-    SDL_PumpEvents(); // atualiza estado do teclado
-    if (state[SDL_SCANCODE_LEFT]) target.x -= 1;
-    if (state[SDL_SCANCODE_RIGHT]) target.x += 1;
-    if (state[SDL_SCANCODE_UP]) target.y -= 1;
-    if (state[SDL_SCANCODE_DOWN]) target.y += 1;
+Controller::~Controller(){}
 
+bool Controller::get_on(){return this->on;}
 
-    while (SDL_PollEvent(&evento)) {
-      if (evento.type == SDL_QUIT) {
-        rodando = false;
-      }
-      //view render()
+void Controller::polling(){
+  // Polling de eventos
+  SDL_PumpEvents(); // atualiza estado do teclado
+  if (state[SDL_SCANCODE_LEFT]){
+    this->model.set_x(this->model.get_x() - 1);
+  }
+  if (state[SDL_SCANCODE_RIGHT]){
+    this->model.set_x(this->model.get_x() + 1);
+  }
+  if (state[SDL_SCANCODE_UP]){
+    this->model.set_y(this->model.get_y() - 1);
+  }
+  if (state[SDL_SCANCODE_DOWN]){
+    this->model.set_y(this->model.get_y() + 1);
+  }
+
+  while (SDL_PollEvent(&(this->event))) {
+    if (this->event.type == SDL_QUIT) {
+      this->on = false;
     }
   }
+}
+
+int main() {
+  Model model = Model();
+  View view = View(model);
+  Controller controller = Controller(model);
+
+  while(controller.get_on()) {
+    controller.polling();
+    view.render();
+  }
+
+  model.~Model();
+  view.~View();
+  controller.~Controller();
+
   return 0;
 }
