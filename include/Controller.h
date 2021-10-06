773f1ef1ce1cd989804b1325a67fbe045b97f9d8
JoaoVQD <89041633+JoaoVQD@users.noreply.github.com>
2021-10-06 17:04:08 -0300

    Update Controller.h

---
+++ include/Controller.h
@@ -0,0 +1,19 @@
+#include <SDL2/SDL_image.h>
+#include "../include/Model.h"
+#include "../include/View.h"
+
+using namespace std;
+
+class Controller{
+  private:
+    bool on;
+    SDL_Event event;
+    Model &model;
+    const Uint8* state;
+  public:
+    Controller(Model &model);
+    ~Controller();
+    void polling();
+    bool get_on();
+    void overlap(float box_x, float box_y, int num);
+};
